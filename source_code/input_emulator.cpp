9eb2d1dcdacb093749c9577994329c595cf39b1c
Wirena <62119056+Wirena@users.noreply.github.com>
2020-05-30 02:52:58 +0300

    aurora crash hotfix
    Yep yep yep, im sending ctrl+s keystroke right after the main message.
        it works on my machine ;) but some crashes before ctrl+s sequence was sent were reported
        dont really know the nature of this behavior, perhaps it depends on amount of time aurora needs to handle input message,
        the less it takes, the more chances it would proccess all message before it notices high type speed

--- source_code/input_emulator.cpp
+++ source_code/input_emulator.cpp
@@ -1,39 +1,5 @@
 #include "input_emulator.h"
 
-
-int InputEmulator::InputTextTmpVer(const TCHAR *text, unsigned length) {
-    textLength = length;
-    this->text = text;
-    AnalyzeText();
-    keys.require(resultKeystokesAmount);
-    keys.zeroAll();
-    INPUT *ptr = keys.getPtr();
-    unsigned keysIterator = 0, textIterator = 0;
-    TCHAR currentChar, lastChar = 0x1000; //0x1000 is not tab, space, cr or rf
-    while (textIterator < length) {
-        currentChar = *(text + textIterator);
-        if (!(currentChar == TAB_SYMBOL ||
-              ((lastChar == CR_SYMBOL || lastChar == LF_SYMBOL) && currentChar == SPACE_SYMBOL))) {
-            AddCharToKeys(ptr + keysIterator, currentChar);
-            keysIterator += 2;
-            lastChar = currentChar;
-        }
-        textIterator++;
-    }
-    unsigned leftOver = resultKeystokesAmount % SENDINPUT_LIMIT;
-    unsigned fullPacks = resultKeystokesAmount / SENDINPUT_LIMIT;
-    unsigned i = 0;
-    for (; i < fullPacks; i++) {
-        SendInput(SENDINPUT_LIMIT, ptr + i * SENDINPUT_LIMIT, sizeof(INPUT));
-        Sleep(2000);
-    }
-    if (leftOver)
-        std::cout << SendInput(leftOver, ptr + i * SENDINPUT_LIMIT, sizeof(INPUT)) << std::endl;
-    keys.zeroAll();
-
-}
-
-
 void InputEmulator::FillKeysArray() {
     unsigned textIterator = 0;
     INPUT *ptr = keys.getPtr();
@@ -48,16 +14,54 @@ void InputEmulator::FillKeysArray() {
         }
         textIterator++;
     }
+    /* Yep yep yep, im sending ctrl+s keystroke right after the main message.
+    it works on my machine ;) but some crashes before ctrl+s sequence was sent were reported
+    dont really know the nature of this behavior, perhaps it depends on amount of time aurora needs to handle input message,
+    the less it takes, the more chances it would proccess all message before it notices high type speed*/
+    
+    ptr->type = INPUT_KEYBOARD;
+    ptr->ki.wVk = VK_CONTROL;
+    ptr->ki.wScan = 0;
+    ptr->ki.dwFlags = 0;
+    ptr++;
+
+    ptr->type = INPUT_KEYBOARD;
+    ptr->ki.wScan = L'S';
+    ptr->ki.dwFlags = KEYEVENTF_UNICODE;
+    ptr++;
+    ptr->type = INPUT_KEYBOARD;
+    ptr->ki.wScan = L'S';
+    ptr->ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
+    ptr++;
+    ptr->type = INPUT_KEYBOARD;
+    ptr->ki.wVk = VK_CONTROL;
+    ptr->ki.wScan = 0;
+    ptr->ki.dwFlags = KEYEVENTF_KEYUP;
+    ptr++;
+
 }
 
 void InputEmulator::AddCharToKeys(INPUT *key, TCHAR chr) {
-    key->type = INPUT_KEYBOARD;
-    key->ki.wScan = chr;
-    key->ki.dwFlags = KEYEVENTF_UNICODE;
-    key++;
-    key->type = INPUT_KEYBOARD;
-    key->ki.wScan = chr;
-    key->ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
+    if(chr==TAB_SYMBOL){
+        return;   //gonna try to add formating later, sending tabs in unicode mode hangs Aurora, i have to check other options
+        key->type = INPUT_KEYBOARD;
+        key->ki.wVk = VK_TAB;
+        key->ki.wScan=0;
+        key->ki.dwFlags = 0;
+        key++;
+        key->type = INPUT_KEYBOARD;
+        key->ki.wVk = VK_TAB;
+        key->ki.wScan=0;
+        key->ki.dwFlags = 0;
+    }else {
+        key->type = INPUT_KEYBOARD;
+        key->ki.wScan = chr;
+        key->ki.dwFlags = KEYEVENTF_UNICODE;
+        key++;
+        key->type = INPUT_KEYBOARD;
+        key->ki.wScan = chr;
+        key->ki.dwFlags = KEYEVENTF_UNICODE | KEYEVENTF_KEYUP;
+    }
 }
 
 void InputEmulator::AnalyzeText() {
@@ -72,6 +76,7 @@ void InputEmulator::AnalyzeText() {
         }
     }
     resultKeystokesAmount *= 2;
+    resultKeystokesAmount+=5;//for last ctrl+s;
 }
 
 int InputEmulator::InputText(const TCHAR *text, unsigned length) {
@@ -81,8 +86,9 @@ int InputEmulator::InputText(const TCHAR *text, unsigned length) {
     keys.require(resultKeystokesAmount);
     keys.zeroAll();
     FillKeysArray();
-    for (unsigned i = 0; i < resultKeystokesAmount; i++)
+    for (unsigned i = 0; i < resultKeystokesAmount; i++) {
         AddInputToQueue(keys.getPtr() + i);
+    }
     return 0;
 }
 
@@ -90,7 +96,7 @@ void InputEmulator::AddInputToQueue(INPUT *inp) {
     SendInput(1, inp, sizeof(INPUT));
     if (GetLastError() == ERROR_NOT_ENOUGH_QUOTA) {
         SetLastError(EXIT_SUCCESS);
-        Sleep(10);
+        Sleep(2);
         AddInputToQueue(inp);
     }
 
